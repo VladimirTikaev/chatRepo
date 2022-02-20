e7ff1091e5c57516f70848876009f9a11bc2a8fe
Vladimir Tikaev <vladimirt.215@gmail.com>
2022-02-20 15:41:14 +0300

    MainController refactoring

--- src/main/java/com/overone/controller/MainController.java
+++ src/main/java/com/overone/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.overone.controller;
 
-
-import com.overone.model.Message;
 import com.overone.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,16 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageService messageService;
-
 
     @GetMapping("/hello")
     public String hello() {
@@ -31,6 +24,4 @@ public class MainController {
         model.addAttribute("messages", messageService.getAllMessages());
         return "chat";
     }
-
-
 }
